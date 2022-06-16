13663c244722dc05f715d9e7c6a1af123635f4e8
Hani <haniyahrisdaa@student.telkomuniversity.ac.id>
2022-06-16 22:17:10 +0700

    New

---
+++ src/maven_project/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+
+	public static void main(String[] args) {
+		Shouter shouter = new Shouter();
+		
+		shouter.shout("hurrah");
+	}
+
+}
---
+++ src/maven_project/Shouter.java
@@ -0,0 +1,11 @@
+public class Shouter {
+
+	public void shout(String message) {
+		
+		String shoutMessage = message.toUpperCase() + '!';
+		
+		System.out.println(shoutMessage);
+		
+	}
+	
+}
